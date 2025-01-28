a2f7c2b1b11e9f5fba4c50e9b3ee10eff35542f9
flutter team <saurav.kumar@itechnolabs.tech>
2025-01-28 19:18:56 +0530

    add snackbar

--- lib/common/common_methods/common_method.dart
+++ lib/common/common_methods/common_method.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 /// hide loader
 hideLoader(BuildContext context) {
   Navigator.pop(context);
 }
 
-snackBar({String? label}) {
+snackBar({required String label}) {
   return Fluttertoast.showToast(
     msg: label,
     toastLength: Toast.LENGTH_SHORT,
--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -3,11 +3,9 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spotify_app/common/common_methods/common_method.dart';
 import 'package:spotify_app/core/constant/keys_constants.dart';
 import 'package:spotify_app/data/models/auth/create_user_model.dart';
-
-import '../../../core/constant/keys_constants.dart';
-import '../../models/auth/create_user_model.dart';
 
 abstract class AuthFirebaseService {
   static CollectionReference<Map<String, dynamic>> userAuth =
@@ -49,8 +47,7 @@ class AuthenticationImpl extends AuthFirebaseService {
   }
 
   @override
-  Future<Either<dynamic, dynamic>> SignIn(
-      CreateUserModel createUserModel) async {
+  Future<Either> SignIn(CreateUserModel createUserModel) async {
     try {
       /// get the users
       final userDoc =
@@ -59,7 +56,9 @@ class AuthenticationImpl extends AuthFirebaseService {
         CreateUserModel userModel = CreateUserModel.fromJson(userDoc.data()!);
         if (userModel.password == createUserModel.password) {
           return Right("SignIn was SuccessFully");
-        } else {}
+        } else {
+          snackBar(label: "User and Password is not match");
+        }
       }
     } on FirebaseException catch (e) {
       return Left(e.message);
